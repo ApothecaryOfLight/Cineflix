fe99192dedf54ccdda875a6d85dd0123c13aac16
Ubuntu <ChangerOfWays@gmail.com>
2021-03-12 19:41:21 +0000

    Added script to download movies.

---
+++ get-movies.sh
@@ -0,0 +1,3 @@
+curl https://ia802300.us.archive.org/0/items/ANightintheShow/A_Night_in_the_Show_512kb.mp4 --output ./backend/movies/charlie_chaplin_a_night_in_the_show.mp4
+curl https://ia600906.us.archive.org/11/items/secret_weapon/sherlock_holmes_and_the_secret_weapon_512kb.mp4 --output ./backend/movies/sherlock_holmes_and_the_secret_weapon.mp4
+curl https://ia800208.us.archive.org/29/items/AliceInWonderland1915_503/AliceInWonderland-WithAudio_512kb.mp4 --output ./backend/movies/alice_in_wonderland.mp4
