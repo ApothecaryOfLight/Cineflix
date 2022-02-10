55c60a119a6225e49dff527dba11c469e48d2187
Ubuntu <ChangerOfWays@gmail.com>
2022-02-10 16:39:42 +0000

    Added install commands to install bash script.

--- install.sh
+++ install.sh
@@ -1,46 +1,37 @@
+#!/bin/bash
 ## Install project.
-## Command line:
-##   -i: standalone or unified.
-##   -s: http or https.
+## Command line argument:
+##   install.sh standalone
+##     Will install Cineflix with all dependencies except for NodeJS packages.
+##   install.sh unified
+##     Will install just Cineflix, with all dependencies installed by the calling install script for UnifiedPortfolio.
 
-## Get the command line arguments.
-while getopts i:s: flag
-do
-  case "${flag}" in
-    i) install_mode=${OPTARG};;
-    s) secure_mode=${OPTARG};;
-  esac
-done
-
-## If no command line arguments are supplied, print the options to console and stop.
-if ! ( [[ "$install_mode" = "standalone" || "$install_mode" = "unified" ]] && [[ "$secure_mode" = "secure" || "$secure_mode" = "unsecure" ]] );
+if [[ "$1" = "standalone" ]];
 then
-  echo "Both command line arguments required.";
-  echo "-i";
-  echo "  standalone: Installs just this project.";
-  echo "  unified: Installs this project as well as any dependencies.";
-  echo "-s";
-  echo "  secure: Installs project for HTTPS, opens ports. Doesn't provide certificates!";
-  echo "  unsecure: Installs project for HTTP, opens ports.";
-  exit;
-fi
-
-#if [[ "$install_mode" = "standalone" ]];
-#then
-#  sudo apt-get upgrade -y sudo apt-get update -y
+  echo "Installing Cineflix and all dependencies.";
 
   #==NODEJS==
-#  curl -sL https://deb.nodesource.com/setup_15.x | sudo -E bash -
-#  sudo apt-get install -y nodejs
+  curl -sL https://deb.nodesource.com/setup_16.x | sudo -E bash -
+  sudo apt-get install -y nodejs
+  cd backend && npm i
 
   #==NGINX==
-#  sudo apt-get install nginx -y
-#  sudo ufw allow 'Nginx HTTP'
-#  sudo ufw allow ssh
-#  sudo ufw enable
+  sudo apt-get install nginx -y
+  sudo ufw allow 'Nginx HTTP'
+  sudo ufw allow 'Nginx HTTPS'
+  sudo ufw allow ssh
+  sudo ufw enable
+elif [[ "$1" = "unified" ]];
+then
+  echo "Installing just Cineflix and NodeJS packages."
+  cd backend && npm i
+else
+  echo "Command line argument:";
+  echo "  install.sh standalone";
+  echo "    Will install Cineflix with all dependencies except for NodeJS packages.";
+  echo "  install.sh unified";
+  echo "    Will install just Cineflix, with all dependencies installed by the calling install script for UnifiedPortfolio.";
+fi
 
-  ##TODO: Configure NGINX.
-#fi
-
-#npm i;
-
+##Set up Nginx route.
+cp frontend/cineflix.video.config /etc/nginx/sites-available/
