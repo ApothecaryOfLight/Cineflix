0c70a288a605b468e4bcd6a8bb3014606d644872
ApothecaryOfLight <ChangerOfWays@gmail.com>
2022-03-30 16:24:04 +0000

    Updated run script to work with the new dev/prod command line arguments.

--- run.sh
+++ run.sh
@@ -1,15 +1,15 @@
 cd "${0%/*}"
 if [[ "$1" = "http" ]];
 then
-  echo "const ip = \"ws://${2}:3000\";" > ./frontend/ip_file.js
+  echo "const ip = \"http://${2}:3000\";" > ./frontend/ip_file.js
 elif [[ "$1" = "https" ]];
 then
-  echo "const ip = \"wss://cineflix.video:3000\";" > ./frontend/ip_file.js
+  echo "const ip = \"https://cineflix.video:3000\";" > ./frontend/ip_file.js
 else
   echo "Command line argument:";
-  echo "  run.sh dev";
+  echo "  run.sh http";
   echo "    Will run Cineflix without an SSL/TSL Certificates.";
-  echo "  run.sh prod";
+  echo "  run.sh https";
   echo "    Will run Cineflix with SSL/TSL Certificates.";
   exit -1
 fi
